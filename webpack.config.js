2a92f2e1fe512faa0527928df911a4928710a2a7
Nans THOMAS <nans.thomas@hetic.net>
2016-10-28 15:07:00 +0200

    Set and configure ScrollReveal
    - Create test animation global and hero-title
    - Modifity button Style global et big
    - Test for reveal div in main.scss
    - Fix Header position and z-index
    - Create a parent div for title and button in hero section
    - Add ScrollReveal Package
    - Lint the webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  entry: "./front/js/app.js",
+  entry: './front/js/app.js',
   output: {
-  path: './public',
-  filename: "bundle.js"
+    path: './public',
+    filename: 'bundle.js',
   },
   devServer: {
-      contentBase: "./public"
-  }
+    contentBase: './public',
+  },
 };
