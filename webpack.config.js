5f3efc74d200b85e8be993cfe46a0fcb2f95debf
Nans THOMAS <nans.thomas@hetic.net>
2016-11-18 00:22:59 +0100

    :heavy_plus_sign: html-loader
    - Add a Html Loader
    - Set in webpack.config.js
    - Add Home.html with test content

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var autoprefixer = require('autoprefixer');
 
 module.exports = {
@@ -7,27 +9,31 @@ module.exports = {
     main: './front/js/app.js'
   },
   output: {
-      path: __dirname + '/public',
-      filename: "bundle.js"
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
         test: /\.html$/,
-        loader: "html-loader"
+        loader: 'html-loader'
       },
       {
-      	test: /\.scss$/,
-      	loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss!sass-loader")
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss!sass-loader')
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader?name=assets/img-[hash:6].[ext]'
       }
     ]
-	},
+  },
   plugins: [
-      new ExtractTextPlugin("style.css")
+    new HtmlWebpackPlugin({
+      title: 'BuddyTrain',
+      template: 'front/templates/home.html'
+    }),
+    new ExtractTextPlugin('style.css')
   ],
   postcss: [ autoprefixer({ browsers: ['last 3 versions'] }) ]
-}
+};
