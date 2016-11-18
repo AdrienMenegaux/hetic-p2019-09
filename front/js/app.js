2a2312c9ad2dcfa3bd9ca3766dec05ec896b23f9
Nans THOMAS <nans.thomas@hetic.net>
2016-11-18 03:02:23 +0100

    :rocket: Test & Play with ScrollMagic
    - Initialise a Controller
    - Build a Tween
    - Build a Scene
    - Triger & Target

--- front/js/app.js
+++ front/js/app.js
@@ -1,2 +1,31 @@
+import ScrollMagic from 'scrollmagic';
+import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
+import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
+import TweenMax from 'gsap';
+
 require('../sass/main.scss');
-var scrollmagic = require('scrollmagic');
+
+// Init Controller
+const controller = new ScrollMagic.Controller({
+  vertical: false,
+});
+
+// Build the Tween
+const tween = [
+  TweenMax
+  .to('#title', 1, { // the statut on end trigger // A QUOI SERT LE 10
+    left: '+=200',
+  }),
+  TweenMax
+  .to('#target', 1, { // the statut on end trigger // A QUOI SERT LE 10
+    height: '80vh',
+  }),
+];
+// Build the Scene
+const scene = new ScrollMagic.Scene({
+  triggerElement: '#title2', // Start Trigger
+  duration: 300, // Distance between start and end
+})
+  .setTween(tween)
+  .addIndicators()
+  .addTo(controller);
